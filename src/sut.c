7e496dc5fc6cc0f742c383a0a9aca8099732aab2
Junjie Yang <junjie.yang2@mail.mcgill.ca>
2020-10-22 17:02:34 -0400

    first commit

---
+++ src/sut.c
@@ -0,0 +1,38 @@
+#include <stdbool.h>
+#include "sut.h"
+
+void sut_init(){
+
+}
+
+bool sut_create(sut_task_f fn){
+
+}
+
+void sut_yield(){
+
+}
+
+void sut_exit(){
+
+}
+
+void sut_open(char *dest, int port){
+
+}
+
+void sut_write(char *buf, int size){
+
+}
+
+void sut_close(){
+
+}
+
+char *sut_read(){
+
+}
+
+void sut_shutdown(){
+    
+}
